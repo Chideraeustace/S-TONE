471ce4b905b01938992acd44a0ea0afb21295a16
Chideraeustace <chideraeutace99@gmail.com>
2025-09-27 08:56:43 +0000

    added the domain

--- src/components/Carts.js
+++ src/components/Carts.js
@@ -302,7 +302,7 @@ const Cart = () => {
                   </div>
                   <div>
                     <label className="block text-sm font-medium text-gray-700">
-                      Location
+                      Country
                     </label>
                     <input
                       type="text"
